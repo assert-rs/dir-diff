0ad3fe61fac97b4936bc8ff8cdd74700a90e9b42
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2018-08-06 19:48:12 +0100

    Feat(Dir/Difference): See Difference Between Dir Files & Content

--- src/lib.rs
+++ src/lib.rs
@@ -62,7 +62,7 @@ pub fn is_different<A: AsRef<Path>, B: AsRef<Path>>(a_base: A, b_base: B) -> Res
 }
 
 macro_rules! add_row {
-    (&mut $table:expr, $file_name:expr, $line_one:expr, $line_two:expr) => {
+    ($table:expr, $file_name:expr, $line_one:expr, $line_two:expr) => {
         $table.add_row(Row::new(vec![
             Cell::new($file_name, 1),
             Cell::new($line_one, 1),
@@ -72,46 +72,114 @@ macro_rules! add_row {
 }
 
 pub fn see_difference<A: AsRef<Path>, B: AsRef<Path>>(a_base: A, b_base: B) -> Result<(), Error> {
-    let mut a_walker = walk_dir(a_base);
-    let mut b_walker = walk_dir(b_base);
-
     let mut table = Table::new();
     table.max_column_width = 40;
 
     table.style = TableStyle::extended();
 
+    let filename_a = &a_base.as_ref().to_str().unwrap();
+    let filename_b = &b_base.as_ref().to_str().unwrap();
+
     table.add_row(Row::new(vec![Cell::new_with_alignment(
-        "Differences",
+        "DIFFERENCES",
         3,
         Alignment::Center,
     )]));
 
-    for (a, b) in (&mut a_walker).zip(&mut b_walker) {
-        let a = a?;
-        let b = b?;
+    table.add_row(Row::new(vec![
+        Cell::new("Filename", 1),
+        Cell::new(filename_a, 1),
+        Cell::new(filename_b, 1),
+    ]));
 
-        let lines = BufReader::new(File::open(b.path())?)
-            .lines()
-            .zip(BufReader::new(File::open(a.path())?).lines());
+    let zipped_file_names = zip_dir_files_to_same_name(
+        &walk_dir_and_get_only_files(&a_base),
+        &mut walk_dir_and_get_only_files(&b_base),
+    );
 
-        for (line_for_a, line_for_b) in lines {
-            match (line_for_a, line_for_b) {
-                (Ok(content_a), Ok(content_b)) => if content_a != content_b {
-                    add_row!(
-                        &mut table,
-                        a.path().to_string_lossy(),
-                        &content_a,
-                        &content_b
-                    );
-                },
-                (Ok(content_a), Err(_)) => {
-                    add_row!(&mut table, a.path().to_string_lossy(), &content_a, "");
+    for (a, b) in zipped_file_names.into_iter() {
+        match (a, b) {
+            (Some(i), None) => {
+                add_row!(table, i, "FILE EXISTS", "DOESN'T EXIST");
+            }
+
+            (None, Some(i)) => {
+                add_row!(table, i, "DOESN'T EXIST", "FILE EXISTS");
+            }
+
+            (Some(file_1), Some(file_2)) => {
+                let mut buffreader_a =
+                    BufReader::new(File::open(format!("{}/{}", filename_a, &file_1))?).lines();
+
+                let mut buffreader_b =
+                    BufReader::new(File::open(format!("{}/{}", filename_b, &file_2))?).lines();
+
+                let mut line_number = 1;
+
+                loop {
+                    match (&buffreader_a.next(), &buffreader_b.next()) {
+                        (None, None) => break,
+
+                        (Some(line_a), Some(line_b)) => {
+                            match (line_a, line_b) {
+                                (Ok(content_a), Ok(content_b)) => if content_a != content_b {
+                                    add_row!(
+                                        table,
+                                        format!("\"{}\":{}", &file_1, line_number),
+                                        &content_a,
+                                        &content_b
+                                    );
+                                },
+                                (Ok(content_a), Err(_)) => {
+                                    add_row!(
+                                        table,
+                                        format!("\"{}\":{}", &file_1, line_number),
+                                        &content_a,
+                                        ""
+                                    );
+                                }
+                                (Err(_), Ok(content_b)) => {
+                                    add_row!(
+                                        table,
+                                        format!("\"{}\":{}", &file_1, line_number),
+                                        "",
+                                        &content_b
+                                    );
+                                }
+                                _ => {}
+                            };
+                        }
+
+                        (Some(line_a), None) => match line_a {
+                            Ok(line_content) => add_row!(
+                                table,
+                                format!("\"{}\":{}", &file_1, line_number),
+                                &line_content,
+                                ""
+                            ),
+
+                            Err(_) => {
+                                add_row!(table, format!("\"{}\":{}", &file_1, line_number), "", "")
+                            }
+                        },
+
+                        (None, Some(line_b)) => match line_b {
+                            Ok(line_content) => add_row!(
+                                table,
+                                format!("\"{}\":{}", &file_2, line_number),
+                                "",
+                                &line_content
+                            ),
+                            Err(_) => {
+                                add_row!(table, format!("\"{}\":{}", &file_2, line_number), "", "")
+                            }
+                        },
+                    };
+
+                    line_number += 1;
                 }
-                (Err(_), Ok(content_b)) => {
-                    add_row!(&mut table, a.path().to_string_lossy(), "", &content_b);
-                }
-                _ => {}
-            };
+            }
+            _ => {}
         }
     }
 
@@ -126,8 +194,42 @@ fn walk_dir<P: AsRef<Path>>(path: P) -> std::iter::Skip<walkdir::IntoIter> {
         .skip(1)
 }
 
+fn walk_dir_and_get_only_files<P: AsRef<Path>>(path: P) -> Vec<String> {
+    WalkDir::new(&path)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|a| a.file_type().is_file())
+        .into_iter()
+        .map(|e| {
+            String::from(e.path().to_str().unwrap()).replace(path.as_ref().to_str().unwrap(), "")
+        })
+        .collect()
+}
+
 fn compare_by_file_name(a: &DirEntry, b: &DirEntry) -> Ordering {
     a.file_name().cmp(b.file_name())
+}
+
+fn zip_dir_files_to_same_name<'a>(
+    el1: &[String],
+    el2: &mut Vec<String>,
+) -> Vec<(Option<String>, Option<String>)> {
+    let matched_data = el1.iter().fold(
+        Vec::<(Option<String>, Option<String>)>::new(),
+        |mut previous, current| {
+            match el2.into_iter().position(|x| x == current) {
+                Some(i) => previous.push((Some(current.to_string()), Some(el2.remove(i)))),
+                None => previous.push((Some(current.to_string()), None)),
+            };
+
+            return previous;
+        },
+    );
+
+    el2.into_iter().fold(matched_data, |mut previous, current| {
+        previous.push((None, Some(current.to_string())));
+        previous
+    })
 }
 
 fn read_to_vec<P: AsRef<Path>>(file: P) -> Result<Vec<u8>, std::io::Error> {
